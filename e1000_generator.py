1fc7bfaea2a9c30eb49bc62a8ed1b7bee20594ea
rakhimov <ol.rakhimov@gmail.com>
2016-01-11 07:46:21 -0800

    Fix style issues found by Prospector

--- e1000_generator.py
+++ e1000_generator.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-"""e1000_generator.py
+"""Generate random initial chess positions.
 
-Code for generation random initial chess positions. The white and black
-positions can be asymmetric; thus, there are overall 960 * 960 = 921600
-valid initial positions. In hex, it is e1000.
+The white and black positions can be asymmetric;
+thus, there are overall 960 * 960 = 921600 valid initial positions.
+In hex, it is e1000.
 """
 
-from pprint import pprint
+from __future__ import print_function
+
 from random import randint, sample
+
 
 def chess_960(color="white"):
     """Generates starting positions according to the rules of Chess960.
@@ -20,15 +22,15 @@ def chess_960(color="white"):
     """
     assert color == "white" or color == "black"
     pieces = {
-            "bishop_dark": "",
-            "bishop_white": "",
-            "knight_one": "",
-            "knight_two": "",
-            "queen": "",
-            "rook_one": "",
-            "rook_two": "",
-            "king": ""
-            }
+        "bishop_dark": "",
+        "bishop_white": "",
+        "knight_one": "",
+        "knight_two": "",
+        "queen": "",
+        "rook_one": "",
+        "rook_two": "",
+        "king": ""
+        }
     empty_squares = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
     # Determine positions of bishops. These are the only pieces picky about the
@@ -61,6 +63,7 @@ def chess_960(color="white"):
 
     return pieces
 
+
 def chess_e1000():
     """Generates asymmetric Chess960 initial positions.
 
@@ -68,6 +71,7 @@ def chess_e1000():
         Dictionary of piece colors and their initial positions.
     """
     return {"white": chess_960("white"), "black": chess_960("black")}
+
 
 def initial_position_to_text(init_setup):
     """Prints initial setup positions into a text.
@@ -80,27 +84,27 @@ def initial_position_to_text(init_setup):
     """
     # NOTE: Unicode colors seem to be reversed in Python for chess pieces.
     whites = {
-            "bishop_dark": u"\u2657",
-            "bishop_white": u"\u2657",
-            "knight_one": u"\u2658",
-            "knight_two": u"\u2658",
-            "queen": u"\u2655",
-            "rook_one": u"\u2656",
-            "rook_two": u"\u2656",
-            "king": u"\u2654",
-            }
+        "bishop_dark": u"\u2657",
+        "bishop_white": u"\u2657",
+        "knight_one": u"\u2658",
+        "knight_two": u"\u2658",
+        "queen": u"\u2655",
+        "rook_one": u"\u2656",
+        "rook_two": u"\u2656",
+        "king": u"\u2654",
+        }
     white_pawn = u"\u2659"
 
     blacks = {
-            "bishop_dark": u"\u265D",
-            "bishop_white": u"\u265D",
-            "knight_one": u"\u265E",
-            "knight_two": u"\u265E",
-            "queen": u"\u265B",
-            "rook_one": u"\u265C",
-            "rook_two": u"\u265C",
-            "king": u"\u265A",
-            }
+        "bishop_dark": u"\u265D",
+        "bishop_white": u"\u265D",
+        "knight_one": u"\u265E",
+        "knight_two": u"\u265E",
+        "queen": u"\u265B",
+        "rook_one": u"\u265C",
+        "rook_two": u"\u265C",
+        "king": u"\u265A",
+        }
     black_pawn = u"\u265F"
 
     def order_setup(piece_positions):
@@ -118,14 +122,14 @@ def initial_position_to_text(init_setup):
                     break
         return ret
 
-    text = ["8 " + \
-            " ".join([whites[x] for x in order_setup(init_setup["black"])])]+ \
-           ["7" + 8 * u" \u2659"] + \
-           [str(i) + " . . . . . . . ." for i in reversed(range(3, 7))] + \
-           ["2" + 8 * u" \u265F"] + \
-           ["1 " + \
-            " ".join([blacks[x] for x in order_setup(init_setup["white"])])]+ \
-           ["  a b c d e f g h", ]
+    text = ["8 " +
+            " ".join([whites[x] for x in order_setup(init_setup["black"])]),
+            "7" + 8 * u" \u2659"]
+    text += [str(i) + " . . . . . . . ." for i in reversed(range(3, 7))]
+    text += ["2" + 8 * u" \u265F",
+             "1 " +
+             " ".join([blacks[x] for x in order_setup(init_setup["white"])]),
+             "  a b c d e f g h"]
     return "\n".join(text)
 
 if __name__ == "__main__":
